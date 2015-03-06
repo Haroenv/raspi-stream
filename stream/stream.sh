1fc5c927da5b7034a23a5b64506c295b80b3d11a
Haroen Viaene <fingebimus@me.com>
2015-03-06 21:30:14 +0100

    made possible to change url

--- stream/stream.sh
+++ stream/stream.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+regex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]' #regex should only allow twitch.tv etc
+echo "What stream do you want to view?"
+read stream
 
-STREAMURL=http://www.twitch.tv/djfluffy_vs_joe
+if [[ $stream =~ $regex ]]
+then 
+    STREAMURL=stream
+else
+    STREAMURL=http://www.twitch.tv/djfluffy_vs_joe #for some reason this is the default
+fi
+
 STREAMRES=best # Stream quality | options: 480p, 720p, 1080p, best
 
 while true
